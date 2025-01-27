65aed8dee946fa030541a970a3c0217d050eaad7
Nithien SA <itnitinhere.05@gmail.com.com>
2025-01-27 09:53:26 +0530

    initial

--- src/pages/indexpage.js
+++ src/pages/indexpage.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Routes, Switch} from "react-router-dom";
 const IndexPage = () => {
     return (
         <div>
-
+            <h1>Index Page</h1>
         </div>
     );
 };
